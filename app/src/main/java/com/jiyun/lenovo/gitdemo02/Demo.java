6fe0f66cd07b652a7081aa30dc1a09e63a1fb4db
liurongb <1270969067@qq.com>
2017-08-14 21:20:03 +0800

    第三场第二镜 action

---
+++ app/src/main/java/com/jiyun/lenovo/gitdemo02/Demo.java
@@ -0,0 +1,13 @@
+package com.jiyun.lenovo.gitdemo02;
+
+import android.util.Log;
+
+/**
+ * Created by lenovo on017/8/14.
+ */
+
+public class Demo {
+ public  void sendGet(){
+     Log.e("TAG","这个就这么难？");
+ }
+}
